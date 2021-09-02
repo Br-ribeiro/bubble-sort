740648f26fd7982c0ceff95506989cf8501e3a89
Bernardo Ribeiro <bernardosr25@gmail.com>
2021-09-01 23:01:17 -0300

    Realizando a ordenação de um array.

---
+++ Sort/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = EncherOVetor(50);
+
+            Console.WriteLine("Array antes de ordenar:");
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.Write(vetor[i] + " ");
+            }
+
+            int temporario;
+
+            for (int j = 0; j <= vetor.Length - 2; j++)
+            {
+                for (int i = 0; i <= vetor.Length - 2; i++)
+                {
+                    if (vetor[i] > vetor[i + 1])
+                    {
+                        temporario = vetor[i + 1];
+                        vetor[i + 1] = vetor[i];
+                        vetor[i] = temporario;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nArray depois de ordenado:");
+
+            foreach (int i in vetor)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.Read();
+        }
+
+
+        public static int[] EncherOVetor(int tamanhoDoArray)
+        {
+            int[] vetorCheio = new int[tamanhoDoArray];
+            Random rnd = new Random();
+
+            for (int i = 0; i < tamanhoDoArray; i++)
+            {
+
+                vetorCheio[i] = rnd.Next(1, 100);
+            }
+
+            return vetorCheio;
+        }
+
+    }
+}
